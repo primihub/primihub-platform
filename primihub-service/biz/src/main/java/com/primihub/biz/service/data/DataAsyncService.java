2a0a5d0032a2c9611bf9359cb387904e2e3300c4
Like habits <17801066560@163.com>
2022-10-14 14:06:51 +0800

    fix(model):update model api

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataAsyncService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataAsyncService.java
@@ -183,76 +183,81 @@ public class DataAsyncService implements ApplicationContextAware {
     public void psiGrpcRun(DataPsiTask psiTask, DataPsi dataPsi){
         DataResource ownDataResource = dataResourceRepository.queryDataResourceById(dataPsi.getOwnResourceId());
         String resourceId,resourceColumnNameList;
+        Integer available;
         if (dataPsi.getOtherOrganId().equals(organConfiguration.getSysLocalOrganId())){
             DataResource otherDataResource = dataResourceRepository.queryDataResourceById(Long.parseLong(dataPsi.getOtherResourceId()));
             resourceId = StringUtils.isNotBlank(otherDataResource.getResourceFusionId())?otherDataResource.getResourceFusionId():otherDataResource.getResourceId().toString();
             resourceColumnNameList = otherDataResource.getFileHandleField();
+            available = 0;
         }else {
             BaseResultEntity dataResource = fusionResourceService.getDataResource(dataPsi.getServerAddress(), dataPsi.getOtherResourceId());
             if (dataResource.getCode()!=0)
                 return;
             Map<String, Object> otherDataResource = (LinkedHashMap)dataResource.getResult();
-            resourceId = otherDataResource.getOrDefault("resourceId","").toString();
+            resourceId = otherDataResource.getOrDefault("resourceId","1").toString();
             resourceColumnNameList = otherDataResource.getOrDefault("resourceColumnNameList","").toString();
+            available = Integer.parseInt(otherDataResource.getOrDefault("available","1").toString());
         }
-
-        Date date=new Date();
-        StringBuilder sb=new StringBuilder().append(baseConfiguration.getResultUrlDirPrefix()).append(DateUtil.formatDate(date,DateUtil.DateStyle.HOUR_FORMAT_SHORT.getFormat())).append("/").append(psiTask.getTaskId()).append(".csv");
-        psiTask.setFilePath(sb.toString());
-        PushTaskReply reply = null;
-        try {
-            log.info("grpc run dataPsiId:{} - psiTaskId:{} - outputFilePath{} - time:{}",dataPsi.getId(),psiTask.getId(),psiTask.getFilePath(),System.currentTimeMillis());
-            Common.ParamValue clientDataParamValue=Common.ParamValue.newBuilder().setValueString(ownDataResource.getResourceFusionId()).build();
-            Common.ParamValue serverDataParamValue=Common.ParamValue.newBuilder().setValueString(resourceId).build();
-            Common.ParamValue psiTypeParamValue=Common.ParamValue.newBuilder().setValueInt32(dataPsi.getOutputContent()).build();
-            Common.ParamValue psiTagParamValue=Common.ParamValue.newBuilder().setValueInt32(dataPsi.getTag()).build();
-            int clientIndex = Arrays.asList(ownDataResource.getFileHandleField().split(",")).indexOf(dataPsi.getOwnKeyword());
-            Common.ParamValue clientIndexParamValue=Common.ParamValue.newBuilder().setValueInt32(clientIndex).build();
-            int serverIndex = Arrays.asList(resourceColumnNameList.split(",")).indexOf(dataPsi.getOtherKeyword());
-            Common.ParamValue serverIndexParamValue=Common.ParamValue.newBuilder().setValueInt32(serverIndex).build();
-            Common.ParamValue outputFullFilenameParamValue=Common.ParamValue.newBuilder().setValueString(psiTask.getFilePath()).build();
-            Common.Params params=Common.Params.newBuilder()
-                    .putParamMap("clientData",clientDataParamValue)
-                    .putParamMap("serverData",serverDataParamValue)
-                    .putParamMap("psiType",psiTypeParamValue)
-                    .putParamMap("psiTag",psiTagParamValue)
-                    .putParamMap("clientIndex",clientIndexParamValue)
-                    .putParamMap("serverIndex",serverIndexParamValue)
-                    .putParamMap("outputFullFilename",outputFullFilenameParamValue)
-                    .build();
-            Common.Task task= Common.Task.newBuilder()
-                    .setType(Common.TaskType.PSI_TASK)
-                    .setParams(params)
-                    .setName("testTask")
-                    .setLanguage(Common.Language.PROTO)
-                    .setCode("import sys;")
-                    .setJobId(ByteString.copyFrom(dataPsi.getId().toString().getBytes(StandardCharsets.UTF_8)))
-                    .setTaskId(ByteString.copyFrom(psiTask.getId().toString().getBytes(StandardCharsets.UTF_8)))
-                    .addInputDatasets("clientData")
-                    .addInputDatasets("serverData")
-                    .build();
-            log.info("grpc Common.Task : \n{}",task.toString());
-            PushTaskRequest request=PushTaskRequest.newBuilder()
-                    .setIntendedWorkerId(ByteString.copyFrom("1".getBytes(StandardCharsets.UTF_8)))
-                    .setTask(task)
-                    .setSequenceNumber(11)
-                    .setClientProcessedUpTo(22)
-                    .build();
-            psiTask.setTaskState(2);
-            dataPsiPrRepository.updateDataPsiTask(psiTask);
-            reply = workGrpcClient.run(o -> o.submitTask(request));
-            log.info("grpc结果:"+reply);
-            DataPsiTask task1 = dataPsiRepository.selectPsiTaskById(psiTask.getId());
-            if (task1.getTaskState()!=4){
-                psiTask.setTaskState(1);
+        if (available==1){
+            Date date=new Date();
+            StringBuilder sb=new StringBuilder().append(baseConfiguration.getResultUrlDirPrefix()).append(DateUtil.formatDate(date,DateUtil.DateStyle.HOUR_FORMAT_SHORT.getFormat())).append("/").append(psiTask.getTaskId()).append(".csv");
+            psiTask.setFilePath(sb.toString());
+            PushTaskReply reply = null;
+            try {
+                log.info("grpc run dataPsiId:{} - psiTaskId:{} - outputFilePath{} - time:{}",dataPsi.getId(),psiTask.getId(),psiTask.getFilePath(),System.currentTimeMillis());
+                Common.ParamValue clientDataParamValue=Common.ParamValue.newBuilder().setValueString(ownDataResource.getResourceFusionId()).build();
+                Common.ParamValue serverDataParamValue=Common.ParamValue.newBuilder().setValueString(resourceId).build();
+                Common.ParamValue psiTypeParamValue=Common.ParamValue.newBuilder().setValueInt32(dataPsi.getOutputContent()).build();
+                Common.ParamValue psiTagParamValue=Common.ParamValue.newBuilder().setValueInt32(dataPsi.getTag()).build();
+                int clientIndex = Arrays.asList(ownDataResource.getFileHandleField().split(",")).indexOf(dataPsi.getOwnKeyword());
+                Common.ParamValue clientIndexParamValue=Common.ParamValue.newBuilder().setValueInt32(clientIndex).build();
+                int serverIndex = Arrays.asList(resourceColumnNameList.split(",")).indexOf(dataPsi.getOtherKeyword());
+                Common.ParamValue serverIndexParamValue=Common.ParamValue.newBuilder().setValueInt32(serverIndex).build();
+                Common.ParamValue outputFullFilenameParamValue=Common.ParamValue.newBuilder().setValueString(psiTask.getFilePath()).build();
+                Common.Params params=Common.Params.newBuilder()
+                        .putParamMap("clientData",clientDataParamValue)
+                        .putParamMap("serverData",serverDataParamValue)
+                        .putParamMap("psiType",psiTypeParamValue)
+                        .putParamMap("psiTag",psiTagParamValue)
+                        .putParamMap("clientIndex",clientIndexParamValue)
+                        .putParamMap("serverIndex",serverIndexParamValue)
+                        .putParamMap("outputFullFilename",outputFullFilenameParamValue)
+                        .build();
+                Common.Task task= Common.Task.newBuilder()
+                        .setType(Common.TaskType.PSI_TASK)
+                        .setParams(params)
+                        .setName("testTask")
+                        .setLanguage(Common.Language.PROTO)
+                        .setCode("import sys;")
+                        .setJobId(ByteString.copyFrom(dataPsi.getId().toString().getBytes(StandardCharsets.UTF_8)))
+                        .setTaskId(ByteString.copyFrom(psiTask.getId().toString().getBytes(StandardCharsets.UTF_8)))
+                        .addInputDatasets("clientData")
+                        .addInputDatasets("serverData")
+                        .build();
+                log.info("grpc Common.Task : \n{}",task.toString());
+                PushTaskRequest request=PushTaskRequest.newBuilder()
+                        .setIntendedWorkerId(ByteString.copyFrom("1".getBytes(StandardCharsets.UTF_8)))
+                        .setTask(task)
+                        .setSequenceNumber(11)
+                        .setClientProcessedUpTo(22)
+                        .build();
+                psiTask.setTaskState(2);
                 dataPsiPrRepository.updateDataPsiTask(psiTask);
+                reply = workGrpcClient.run(o -> o.submitTask(request));
+                log.info("grpc结果:"+reply);
+                DataPsiTask task1 = dataPsiRepository.selectPsiTaskById(psiTask.getId());
+                if (task1.getTaskState()!=4){
+                    psiTask.setTaskState(1);
 //                psiTaskOutputFileHandle(psiTask);
+                }
+            } catch (Exception e) {
+                psiTask.setTaskState(3);
+                log.info("grpc Exception:{}",e.getMessage());
             }
-        } catch (Exception e) {
+        }else {
             psiTask.setTaskState(3);
-            dataPsiPrRepository.updateDataPsiTask(psiTask);
-            log.info("grpc Exception:{}",e.getMessage());
         }
+        dataPsiPrRepository.updateDataPsiTask(psiTask);
         log.info("grpc end dataPsiId:{} - psiTaskId:{} - outputFilePath{} - time:{}",dataPsi.getId(),psiTask.getId(),psiTask.getFilePath(),System.currentTimeMillis());
     }
     public void psiTaskOutputFileHandle(DataPsiTask task){
