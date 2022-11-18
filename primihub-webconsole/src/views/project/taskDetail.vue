8c8a3511f3147fbb15b7990aa439dc5ec1962d2d
zhanjingjing <zhanjingjing@primihub.com>
2022-11-18 11:05:39 +0800

    fix: model view style error

--- primihub-webconsole/src/views/project/taskDetail.vue
+++ primihub-webconsole/src/views/project/taskDetail.vue
@@ -104,7 +104,7 @@
         </el-tab-pane>
         <el-tab-pane v-if="task.isCooperation === 0 || (task.isCooperation === 1 && task.taskState === 1)" label="预览图" name="3">
           <div class="canvas-panel">
-            <TaskCanvas :model-id="modelId" :options="taskOptions" :model-data="modelComponent" :state="task.taskState" :restart-run="restartRun" @complete="handleTaskComplete" />
+            <TaskCanvas v-if="tabName === '3' && modelId" :model-id="modelId" :options="taskOptions" :model-data="modelComponent" :state="task.taskState" :restart-run="restartRun" @complete="handleTaskComplete" />
           </div>
         </el-tab-pane>
         <el-tab-pane label="日志" name="4">
