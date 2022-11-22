f181c6e096096f2dc7f72c3cca79855fa5646029
Like habits <17801066560@163.com>
2022-11-22 16:16:00 +0800

    add(log):add task log

--- primihub-service/biz/src/main/java/com/primihub/biz/service/sys/SysSseEmitterService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/sys/SysSseEmitterService.java
@@ -40,7 +40,8 @@ public class SysSseEmitterService {
     public void sendMessage(String key, String message) {
         if (CommStorageUtil.getSseEmitterMap().containsKey(key)) {
             try {
-                CommStorageUtil.getSseEmitterMap().get(key).send(message, MediaType.APPLICATION_JSON);
+//                CommStorageUtil.getSseEmitterMap().get(key).send(message, MediaType.APPLICATION_JSON);
+                CommStorageUtil.getSseEmitterMap().get(key).send(message);
 //                sseEmitterMap.get(key).send(message);
             } catch (IOException e) {
                 log.error("标识[{}]推送异常:{}", key, e.getMessage());
