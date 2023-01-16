6b914bbc9cea96465d4d8d0b7f9037547dc85d23
Like habits <17801066560@163.com>
2023-01-16 15:03:41 +0800

    fix(model):update xgb

--- primihub-service/biz/src/main/java/com/primihub/biz/util/DataUtil.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/util/DataUtil.java
@@ -11,14 +11,14 @@ public class DataUtil {
 
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+){0,4}$";
 
-    public static Map<String,String> getJDBCData(String url){
-        Map<String,String> map = new HashMap<>();
+    public static Map<String,Object> getJDBCData(String url){
+        Map<String,Object> map = new HashMap<>();
         Pattern namePattern = Pattern.compile(JDBC_PATTERN);
         Matcher dateMatcher = namePattern.matcher(url);
         while (dateMatcher.find()) {
             map.put("type",dateMatcher.group("type"));
             map.put("host",dateMatcher.group("host"));
-            map.put("port",dateMatcher.group("port"));
+            map.put("port",Integer.valueOf(dateMatcher.group("port")));
             map.put("database",dateMatcher.group("database"));
         }
         return map;
