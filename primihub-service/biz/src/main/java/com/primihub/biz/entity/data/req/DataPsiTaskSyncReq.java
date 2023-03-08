f1651473eddd1f94e9687900c9978c9d2d4cc330
Like habits <17801066560@163.com>
2023-03-08 21:30:16 +0800

    task

--- primihub-service/biz/src/main/java/com/primihub/biz/entity/data/req/DataPsiTaskSyncReq.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/entity/data/req/DataPsiTaskSyncReq.java
@@ -10,13 +10,20 @@ public class DataPsiTaskSyncReq {
     private DataPsiTask psiTask;
     private DataPsi dataPsi;
     private DataTask dataTask;
+    private Long timestamp;
+    private Integer nonce;
 
     public DataPsiTaskSyncReq(DataPsiTask psiTask, DataPsi dataPsi, DataTask dataTask) {
         this.psiTask = psiTask;
         this.dataPsi = dataPsi;
         this.dataTask = dataTask;
+        supplement();
     }
 
     public DataPsiTaskSyncReq() {
+    }
+    public void supplement(){
+        this.timestamp = System.currentTimeMillis();
+        this.nonce = (int)Math.random()*100;
     }
 }
