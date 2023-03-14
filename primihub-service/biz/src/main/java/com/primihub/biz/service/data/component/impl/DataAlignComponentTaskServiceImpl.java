ed84b1a2ec47187fe554f647d1ba6de4e2cd9872
Like habits <17801066560@163.com>
2023-03-14 19:15:42 +0800

    fix(model):DataAlignComponentTaskServiceImpl.java

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/component/impl/DataAlignComponentTaskServiceImpl.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/component/impl/DataAlignComponentTaskServiceImpl.java
@@ -192,18 +192,18 @@ public class DataAlignComponentTaskServiceImpl extends BaseComponentServiceImpl 
                 return BaseResultEntity.failure(BaseResultEnum.DATA_RUN_TASK_FAIL,"数据对齐选择为空");
             List<Integer> clientIndex;
             List<Integer> serverIndex;
-            Stream<String> stream = null;
+            List<String> fieldList = null;
             if ("1".equals(dataAlign)){
-                stream = Arrays.stream(new String[]{"id"});
+                fieldList = Arrays.stream(new String[]{"id"}).collect(Collectors.toList());
             }else {
                 String multipleSelected = componentVals.get("MultipleSelected");
                 if (StringUtils.isBlank(multipleSelected))
                     return BaseResultEntity.failure(BaseResultEnum.DATA_RUN_TASK_FAIL,"数据对齐选择特征为空");
-                stream = Arrays.stream(multipleSelected.split(","));
+                fieldList = Arrays.stream(multipleSelected.split(",")).collect(Collectors.toList());
             }
-            log.info(JSONObject.toJSONString(stream));
-            clientIndex = stream.map(clientData.getFileHandleField()::indexOf).collect(Collectors.toList());
-            serverIndex = stream.map(serverData.getFileHandleField()::indexOf).collect(Collectors.toList());
+            log.info(JSONObject.toJSONString(fieldList));
+            clientIndex = fieldList.stream().map(clientData.getFileHandleField()::indexOf).collect(Collectors.toList());
+            serverIndex = fieldList.stream().map(serverData.getFileHandleField()::indexOf).collect(Collectors.toList());
             log.info("{} - {}",JSONObject.toJSONString(clientIndex),JSONObject.toJSONString(serverIndex));
             if (clientIndex.size()<0)
                 return BaseResultEntity.failure(BaseResultEnum.DATA_RUN_TASK_FAIL,"数据对齐发起方特征未查询到");
