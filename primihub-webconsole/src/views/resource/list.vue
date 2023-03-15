12a74770b179a5a1f9bf640fd85ac8ec1eef7c16
zhanjingjing <zhanjingjing@primihub.com>
2023-03-15 17:55:54 +0800

    resource list button size

--- primihub-webconsole/src/views/resource/list.vue
+++ primihub-webconsole/src/views/resource/list.vue
@@ -138,9 +138,9 @@
           align="center"
         >
           <template slot-scope="{row}">
-            <el-button type="text" size="mini" @click="toResourceDetailPage(row.resourceId)">查看</el-button>
-            <el-button v-if="hasEditPermission && row.resourceState === 0" size="mini" type="text" @click="toResourceEditPage(row.resourceId)">编辑</el-button>
-            <el-button size="mini" type="text" @click="changeResourceStatus(row)">{{ row.resourceState === 0 ? '下线': '上线' }}</el-button>
+            <el-button type="text" @click="toResourceDetailPage(row.resourceId)">查看</el-button>
+            <el-button v-if="hasEditPermission && row.resourceState === 0" type="text" @click="toResourceEditPage(row.resourceId)">编辑</el-button>
+            <el-button type="text" @click="changeResourceStatus(row)">{{ row.resourceState === 0 ? '下线': '上线' }}</el-button>
           </template>
         </el-table-column>
       </el-table>
