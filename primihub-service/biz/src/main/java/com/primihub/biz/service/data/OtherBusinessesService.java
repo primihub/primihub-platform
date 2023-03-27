bc96d73908d6c9dda059b1d51a54c7c0eb323dd0
Like habits <17801066560@163.com>
2023-03-27 18:43:58 +0800

    fix(model):update relieveUserAccount api

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/OtherBusinessesService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/OtherBusinessesService.java
@@ -162,7 +162,7 @@ public class OtherBusinessesService {
 
     public void syncGatewayApiData(Object vo,String gatewayAddressAndApi,String publicKey){
         try {
-            Object data = null;
+            Object data;
             if (StringUtils.isEmpty(publicKey)){
                 data = vo;
             }else {
@@ -173,7 +173,7 @@ public class OtherBusinessesService {
             HttpEntity<HashMap<String, Object>> request = new HttpEntity(data, headers);
             log.info(gatewayAddressAndApi);
             BaseResultEntity baseResultEntity = restTemplate.postForObject(gatewayAddressAndApi, request, BaseResultEntity.class);
-            log.info("baseResultEntity code:{} msg:{}",baseResultEntity.getCode(),baseResultEntity.getMsg());
+            log.info("baseResultEntity {}",JSONObject.toJSONString(baseResultEntity));
         }catch (Exception e){
             log.info("gatewayAddress api url:{} Exception:{}",gatewayAddressAndApi,e.getMessage());
             e.printStackTrace();
