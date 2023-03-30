652c38d8c479a6adb1ef216fe32648c44b502db4
Like habits <17801066560@163.com>
2023-03-30 17:52:55 +0800

    add(sys):add collect api

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/OtherBusinessesService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/OtherBusinessesService.java
@@ -45,7 +45,7 @@ public class OtherBusinessesService {
             map.put("pinCode", new ArrayList(){{add(sysLocalOrganInfo.getPinCode());}});
             map.put("resourceId", new ArrayList(){{add(req.getResourceId());}});
             map.put("resourceName", new ArrayList(){{add(req.getResourceName());}});
-            map.put("resourceType", new ArrayList(){{add(req.getResourceType());}});
+            map.put("resourceType", new ArrayList(){{add(req.getResourceSource());}});
             map.put("organId", new ArrayList(){{add(req.getOrganId());}});
             map.put("fileContainsY", new ArrayList(){{add(req.getFileContainsY());}});
             map.put("tagName", new ArrayList(){{add(req.getTagName());}});
