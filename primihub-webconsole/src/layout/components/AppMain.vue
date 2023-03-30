2ef54c6e04d8ba2265c93e2c324c891e1fa4b915
zhanjingjing <zhanjingjing@primihub.com>
2023-03-30 19:49:06 +0800

    fix: eslint

--- primihub-webconsole/src/layout/components/AppMain.vue
+++ primihub-webconsole/src/layout/components/AppMain.vue
@@ -24,10 +24,10 @@ export default {
     key() {
       return this.$route.path
     },
-    routePath(){
-      let path = this.$store.state.watchRouter.currentPath;
-      if(path.search('/map/index') > -1){
-        return true;
+    routePath() {
+      const path = this.$store.state.watchRouter.currentPath
+      if (path.search('/map/index') > -1) {
+        return true
       }
       return false
     }
