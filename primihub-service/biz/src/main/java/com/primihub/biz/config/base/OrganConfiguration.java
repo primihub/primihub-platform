cbb684bfb731050fd783007809da45549ca2eada
Like habits <17801066560@163.com>
2023-03-30 17:01:47 +0800

    add(sys):add collect api

--- primihub-service/biz/src/main/java/com/primihub/biz/config/base/OrganConfiguration.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/config/base/OrganConfiguration.java
@@ -10,10 +10,14 @@ import com.primihub.biz.entity.sys.po.SysLocalOrganInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -70,7 +74,8 @@ public class OrganConfiguration {
     @PostConstruct
     public void init(){
         readNacosConfigOrganInfo();
-        readNacosConfigCollectOrganInfo();
+//        readNacosConfigCollectOrganInfo();
+        readResourceConfigCollectOrganInfo();
     }
 
 
@@ -109,36 +114,60 @@ public class OrganConfiguration {
         }
     }
 
-    public void readNacosConfigCollectOrganInfo(){
+    public void readResourceConfigCollectOrganInfo(){
         try {
-            String group=environment.getProperty("nacos.config.group");
-            String serverAddr=environment.getProperty("nacos.config.server-addr");
-            Properties properties = new Properties();
-            properties.put("serverAddr",serverAddr);
-            ConfigService configService= NacosFactory.createConfigService(properties);
-            String collectDataContent=configService.getConfig(SysConstant.SYS_COLLECT_DATA_ORGAN_INFO_NAME,group,3000);
-            log.info(" nacos collect_data data:{}",collectDataContent);
-            if (StringUtils.isNotBlank(collectDataContent)){
-                collectOrganList = JSON.parseArray(collectDataContent,SysCollectOrgan.class);
+            org.springframework.core.io.Resource  resource = new ClassPathResource("collectData.json");
+            InputStream is = resource.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String data = null;
+            while((data = br.readLine()) != null) {
+                sb.append(data);
             }
-            configService.addListener(SysConstant.SYS_COLLECT_DATA_ORGAN_INFO_NAME, group, new Listener() {
-                @Override
-                public Executor getExecutor() {
-                    return null;
-                }
-
-                @Override
-                public void receiveConfigInfo(String config) {
-                    log.info(" nacos receiveConfigInfo organ_info data:{}",config);
-                    if (StringUtils.isNotBlank(config)){
-                        collectOrganList = JSON.parseArray(collectDataContent,SysCollectOrgan.class);
-                    }else {
-                        collectOrganList = null;
-                    }
-                }
-            });
+            if (StringUtils.isNotBlank(sb.toString())){
+                log.info("data:{}",sb.toString());
+                collectOrganList = JSON.parseArray(sb.toString(),SysCollectOrgan.class);
+            }
+            br.close();
+            isr.close();
+            is.close();
         }catch (Exception e){
-            log.info("nacos collect_data Exception:{}",e.getMessage());
+            log.info("读取文件失败:{}",e.getMessage());
         }
+
     }
+
+//    public void readNacosConfigCollectOrganInfo(){
+//        try {
+//            String group=environment.getProperty("nacos.config.group");
+//            String serverAddr=environment.getProperty("nacos.config.server-addr");
+//            Properties properties = new Properties();
+//            properties.put("serverAddr",serverAddr);
+//            ConfigService configService= NacosFactory.createConfigService(properties);
+//            String collectDataContent=configService.getConfig(SysConstant.SYS_COLLECT_DATA_ORGAN_INFO_NAME,group,3000);
+//            log.info(" nacos collect_data data:{}",collectDataContent);
+//            if (StringUtils.isNotBlank(collectDataContent)){
+//                collectOrganList = JSON.parseArray(collectDataContent,SysCollectOrgan.class);
+//            }
+//            configService.addListener(SysConstant.SYS_COLLECT_DATA_ORGAN_INFO_NAME, group, new Listener() {
+//                @Override
+//                public Executor getExecutor() {
+//                    return null;
+//                }
+//
+//                @Override
+//                public void receiveConfigInfo(String config) {
+//                    log.info(" nacos receiveConfigInfo organ_info data:{}",config);
+//                    if (StringUtils.isNotBlank(config)){
+//                        collectOrganList = JSON.parseArray(collectDataContent,SysCollectOrgan.class);
+//                    }else {
+//                        collectOrganList = null;
+//                    }
+//                }
+//            });
+//        }catch (Exception e){
+//            log.info("nacos collect_data Exception:{}",e.getMessage());
+//        }
+//    }
 }
