c11902c0ac6ff7741b6fcea4b37b7c2a18eb9b18
zhanjingjing <zhanjingjing@primihub.com>
2023-04-11 17:38:24 +0800

    feat: application read number

--- primihub-webconsole/src/views/applicationMarket/index.vue
+++ primihub-webconsole/src/views/applicationMarket/index.vue
@@ -23,7 +23,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import Navbar from './components/ApplicationNavBar'
 import TopBanner from './components/TopBanner.vue'
 
@@ -40,10 +40,22 @@ export default {
   computed: {
     ...mapState('application', ['data'])
   },
+  created() {
+    this.getReadNumber().then(res => {
+      if (!res) {
+        this.getReadNumber({
+          type: 'PIR,PSI,reasoning,ADPrediction,UserPortrait',
+          operation: 2
+        })
+      }
+    })
+  },
   methods: {
     toDetail(name) {
+      this.getReadNumber({ type: name, operation: 1 })
       this.$router.push(`/applicationIndex/detail/${name}`)
-    }
+    },
+    ...mapActions('application', ['getReadNumber'])
   }
 }
 </script>
