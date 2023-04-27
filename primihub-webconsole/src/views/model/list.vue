3f9a93f090ae6dfb2cc41858dbbdb91d2da7e2a8
zhanjingjing <zhanjingjing@primihub.com>
2023-04-27 16:38:36 +0800

    fix: model search time

--- primihub-webconsole/src/views/model/list.vue
+++ primihub-webconsole/src/views/model/list.vue
@@ -27,6 +27,7 @@
             start-placeholder="开始日期"
             end-placeholder="结束日期"
             value-format="yyyy-MM-dd HH:mm:ss"
+            :default-time="['00:00:00', '23:59:59']"
           />
         </el-form-item>
         <el-form-item>
