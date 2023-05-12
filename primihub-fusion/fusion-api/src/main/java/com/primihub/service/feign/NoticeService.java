8bf7e10fac9d0c5291c8fd8785fcb84820eab96b
Like habits <17801066560@163.com>
2023-05-12 14:05:59 +0800

    update fusion

--- primihub-fusion/fusion-api/src/main/java/com/primihub/service/feign/NoticeService.java
+++ primihub-fusion/fusion-api/src/main/java/com/primihub/service/feign/NoticeService.java
@@ -3,10 +3,11 @@ package com.primihub.service.feign;
 import com.primihub.entity.base.BaseResultEntity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "platform",contextId = "resource")
 public interface NoticeService {
 
     @PostMapping("/resource/noticeResource")
-    BaseResultEntity noticeResource(String resourceId);
+    BaseResultEntity noticeResource(@RequestParam("resourceId") String resourceId);
 }
