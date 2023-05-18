9a95b260cda5f020427bb68284f41288a174a607
Like habits <17801066560@163.com>
2023-05-18 15:24:45 +0800

    update fusion

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/component/impl/ModelComponentTaskServiceImpl.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/component/impl/ModelComponentTaskServiceImpl.java
@@ -337,8 +337,8 @@ public class ModelComponentTaskServiceImpl extends BaseComponentServiceImpl impl
                         .putParamMap("component_params", componentParamsParamValue)
                         .build();
                 Map<String, Common.Dataset> values = new HashMap<>();
-                values.put("Bob",Common.Dataset.newBuilder().putData("Data_File",taskReq.getFreemarkerMap().get("label_dataset")).build());
-                values.put("Charlie",Common.Dataset.newBuilder().putData("Data_File",taskReq.getFreemarkerMap().get("guest_dataset")).build());
+                values.put("Bob",Common.Dataset.newBuilder().putData("data_set",taskReq.getFreemarkerMap().get("label_dataset")).build());
+                values.put("Charlie",Common.Dataset.newBuilder().putData("data_set",taskReq.getFreemarkerMap().get("guest_dataset")).build());
                 Common.TaskContext taskBuild = Common.TaskContext.newBuilder().setJobId(jobId).setRequestId(String.valueOf(SnowflakeId.getInstance().nextId())).setTaskId(taskReq.getDataTask().getTaskIdName()).build();
                 Common.Task task = Common.Task.newBuilder()
                         .setType(Common.TaskType.ACTOR_TASK)
