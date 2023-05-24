e2ea022b47546d8a7d5cc918002337dd71592340
fenghaolun <1120560948@qq.com>
2023-05-24 13:44:01 +0800

    fix

--- primihub-service/biz/src/main/java/com/primihub/biz/constant/DataConstant.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/constant/DataConstant.java
@@ -17,8 +17,8 @@ public class DataConstant {
     public final static String FREEMARKER_PYTHON_HOMO_NN_BINARY_PATH= "homo_nn_binary.ftl";
     //public final static String FREEMARKER_PYTHON_HOMO_NN_BINARY_INFER_PATH= "homo_nn_binary_infer.ftl";
 
-    //public final static String FREEMARKER_PYTHON_EXCEPTION_PATH= "exception.ftl";
-    //public final static String FREEMARKER_PYTHON_DATA_ALIGN_PATH= "data_align.ftl";
+    public final static String FREEMARKER_PYTHON_EXCEPTION_PATH= "exception.ftl";
+    public final static String FREEMARKER_PYTHON_DATA_ALIGN_PATH= "data_align.ftl";
     public final static String[] FREEMARKER_PYTHON_PATHS = new String[]{
             FREEMARKER_PYTHON_EN_PATH,
             //FREEMARKER_PYTHON_HOMO_XGB_INFER_PATH,
@@ -28,8 +28,8 @@ public class DataConstant {
             //FREEMARKER_PYTHON_HETERO_LR_INFER_PATH,
             FREEMARKER_PYTHON_HOMO_NN_BINARY_PATH,
             //FREEMARKER_PYTHON_HOMO_NN_BINARY_INFER_PATH,
-            //FREEMARKER_PYTHON_EXCEPTION_PATH,
-            //FREEMARKER_PYTHON_DATA_ALIGN_PATH,
+            FREEMARKER_PYTHON_EXCEPTION_PATH,
+            FREEMARKER_PYTHON_DATA_ALIGN_PATH,
     };
     // python dataset host、guest
     public final static String PYTHON_LABEL_DATASET = "label_dataset";
