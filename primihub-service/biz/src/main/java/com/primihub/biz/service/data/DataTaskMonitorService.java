7debbd045c6e08c01eb4507073629f50e2fb8bf9
Like habits <17801066560@163.com>
2023-06-05 10:56:33 +0800

    fix task

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataTaskMonitorService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataTaskMonitorService.java
@@ -32,6 +32,8 @@ public class DataTaskMonitorService {
     @Autowired
     private WorkGrpcClient workGrpcClient;
 
+    private final static Long FILE_VERIFICATION_TIME = 10000L;
+
     public void continuouslyObtainTaskStatus(DataTask dataTask,Common.TaskContext taskBuild,int num,String path){
         boolean isContinue = true;
         primaryStringRedisTemplate.opsForSet().add(RedisKeyConstant.TASK_STATUS_LIST_KEY,String.format("%s-%s-%s",taskBuild.getTaskId(),taskBuild.getJobId(),taskBuild.getRequestId()));
@@ -61,15 +63,31 @@ public class DataTaskMonitorService {
                             log.info("num:{} - success:{}",num,success);
                             if (num <= success){
                                 dataTask.setTaskState(TaskStateEnum.SUCCESS.getStateType());
-                                if (StringUtils.isNotBlank(path)){
-                                    // 暂停10秒 在k8s中文件同步存在一定的延迟性 TODO 物理机可以注释
-                                    Thread.sleep(10000L);
-                                    if (!FileUtil.isFileExists(path)){
+                                //TODO 循环1分钟 因在k8s中文件同步存在一定的延迟性。物理机可以注释【一】打开【二】
+                                // ------------一---------------------
+                                long start = System.currentTimeMillis();
+                                while (true){
+                                    if (StringUtils.isNotBlank(path)){
+                                        if (FileUtil.isFileExists(path)){
+                                            log.info("{} - 存在了-退出",path);
+                                            break;
+                                        }
+                                    }
+                                    if ((System.currentTimeMillis() - start)>FILE_VERIFICATION_TIME){
                                         log.info("path:{} 不存在",path);
                                         dataTask.setTaskState(TaskStateEnum.FAIL.getStateType());
                                         dataTask.setTaskErrorMsg("运行失败:无文件信息");
+                                        break;
                                     }
                                 }
+                                // ------------二---------------------
+//                                if (StringUtils.isNotBlank(path)){
+//                                    if (!FileUtil.isFileExists(path)){
+//                                        log.info("path:{} 不存在",path);
+//                                        dataTask.setTaskState(TaskStateEnum.FAIL.getStateType());
+//                                        dataTask.setTaskErrorMsg("运行失败:无文件信息");
+//                                    }
+//                                }
                                 isContinue = false;
                             }
                         }
