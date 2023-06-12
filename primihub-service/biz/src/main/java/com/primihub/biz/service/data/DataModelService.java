4302dbe5ad49dd85ee8ec848071d0853920d3f07
Like habits <17801066560@163.com>
2023-06-12 16:27:57 +0800

    update project

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataModelService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataModelService.java
@@ -496,7 +496,10 @@ public class DataModelService {
     public BaseResultEntity restartTaskModel(Long taskId) {
         DataTask dataTask = dataTaskRepository.selectDataTaskByTaskId(taskId);
         if (dataTask==null) {
-            return BaseResultEntity.failure(BaseResultEnum.DATA_RUN_TASK_FAIL,"未查询到任务信息");
+            dataTask = dataTaskRepository.selectDataTaskByTaskIdName(taskId.toString());
+            if (dataTask==null){
+                return BaseResultEntity.failure(BaseResultEnum.DATA_RUN_TASK_FAIL,"未查询到任务信息");
+            }
         }
         DataModelTask modelTask = dataModelRepository.queryModelTaskById(taskId);
         if (modelTask==null) {
