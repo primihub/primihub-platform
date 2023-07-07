77989006d78b426c3649df0608c8ef24270e55be
zjj614 <107016099+Zjj614@users.noreply.github.com>
2023-07-07 11:43:07 +0800

    feat: remove console (#154)

--- primihub-webconsole/babel.config.js
+++ primihub-webconsole/babel.config.js
@@ -1,4 +1,11 @@
+const plugins = ['@vue/babel-plugin-transform-vue-jsx']
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+
 module.exports = {
+  plugins: plugins,
   presets: [
     // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
     '@vue/cli-plugin-babel/preset'
