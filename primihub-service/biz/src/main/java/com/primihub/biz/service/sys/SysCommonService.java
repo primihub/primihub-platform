6cd16a3533a5b6f214bb4c5778e65f90f68c038e
Like habits <17801066560@163.com>
2023-08-18 17:51:42 +0800

    update addressinfo

--- primihub-service/biz/src/main/java/com/primihub/biz/service/sys/SysCommonService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/sys/SysCommonService.java
@@ -1,23 +1,31 @@
 package com.primihub.biz.service.sys;
 
+import com.alibaba.fastjson.JSONObject;
 import com.primihub.biz.constant.SysConstant;
 import com.primihub.biz.repository.primaryredis.sys.SysCommonPrimaryRedisRepository;
 import com.primihub.biz.entity.base.BaseResultEntity;
 import com.primihub.biz.entity.base.BaseResultEnum;
 import com.primihub.biz.util.crypt.CryptUtil;
 import com.primihub.biz.util.crypt.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class SysCommonService {
 
     @Autowired
     private SysCommonPrimaryRedisRepository sysCommonPrimaryRedisRepository;
+    @Resource
+    private RestTemplate restTemplate;
 
     public BaseResultEntity getValidatePublicKey() {
         String[] rsaKeyPair;
@@ -40,4 +48,13 @@ public class SysCommonService {
         return BaseResultEntity.success(map);
     }
 
+    public BaseResultEntity getCollectList() {
+        ResponseEntity<JSONObject> ipInfoData = restTemplate.getForEntity(SysConstant.SYS_QUERY_COLLECT_URL, JSONObject.class);
+        log.info("获取客户端ip json:{}",JSONObject.toJSONString(ipInfoData));
+        if (ipInfoData==null || ipInfoData.getBody()==null){
+            return null;
+        }
+        JSONObject body = ipInfoData.getBody();
+        return BaseResultEntity.success(body.getJSONArray("data"));
+    }
 }
