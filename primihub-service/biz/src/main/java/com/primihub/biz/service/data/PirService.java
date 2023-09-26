cd9359d4eba7379787a291d8dccfecbf209035e2
Like habits <17801066560@163.com>
2023-09-26 18:46:23 +0800

    fix pir

--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/PirService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/PirService.java
@@ -15,6 +15,7 @@ import com.primihub.biz.entity.data.vo.DataPirTaskVo;
 import com.primihub.biz.repository.primarydb.data.DataTaskPrRepository;
 import com.primihub.biz.repository.secondarydb.data.DataTaskRepository;
 import com.primihub.biz.util.FileUtil;
+import com.primihub.biz.util.crypt.DateUtil;
 import com.primihub.biz.util.snowflake.SnowflakeId;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -112,14 +113,26 @@ public class PirService {
         Map<String, Object> map = new HashMap<>();
         map.put("taskName", dataTask.getTaskName());
         map.put("taskIdName", dataTask.getTaskIdName());
+        map.put("taskState",dataTask.getTaskState());
         map.put("organName", task.getProviderOrganName());
         map.put("resourceName", task.getResourceName());
         map.put("resourceId", task.getResourceId());
         map.put("retrievalId", task.getRetrievalId());
-        map.put("createDate", task.getCreateDate());
-        map.put("consuming", dataTask.getTaskEndTime() - dataTask.getTaskStartTime());
+        map.put("taskError",dataTask.getTaskErrorMsg());
+        map.put("createDate", DateUtil.formatDate(task.getCreateDate(),DateUtil.DateStyle.TIME_FORMAT_NORMAL.getFormat()));
+        map.put("consuming", getConsuming(dataTask.getTaskStartTime(),dataTask.getTaskEndTime()));
         map.put("createTime", dataTask.getTaskStartTime());
         map.put("dataList", list);
         return BaseResultEntity.success(map);
+    }
+
+    public Long getConsuming(Long taskStart,Long taskEnd) {
+        if (taskStart==null) {
+            return 0L;
+        }
+        if (taskEnd==null||taskEnd==0) {
+            return (System.currentTimeMillis() - taskStart)/1000;
+        }
+        return (taskEnd - taskStart)/1000;
     }
 }
