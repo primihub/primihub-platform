1b4e91b7ac980911d932d78e915df1f41203ad7b
Like habits <17801066560@163.com>
2023-11-07 14:16:01 +0800

    fix source

--- primihub-service/biz/src/main/java/com/primihub/biz/entity/data/dataenum/SourceEnum.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/entity/data/dataenum/SourceEnum.java
@@ -12,7 +12,7 @@ public enum SourceEnum {
     mysql(1,"mysql", MySqlServiceAbstract.class),
     sqlite(2,"sqlite", SqliteServiceAbstract.class),
     hive(3,"hive", OtherServiceAbstract.class),
-    dm(4,"达梦", OtherServiceAbstract.class),
+    dm(4,"dm", OtherServiceAbstract.class),
     sql_server(5,"SqlServer", OtherServiceAbstract.class),
     oracle(6,"oracle", OtherServiceAbstract.class),
     ;
