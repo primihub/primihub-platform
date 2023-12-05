0b9179eb95f2d615212607f2ee2c8fe7a7347c7a
Like habits <17801066560@163.com>
2023-12-05 13:49:42 +0800

    delete resource

--- primihub-service/biz/src/main/java/com/primihub/biz/config/base/SwaggerConfig.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/config/base/SwaggerConfig.java
@@ -2,21 +2,19 @@ package com.primihub.biz.config.base;
 
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
-import com.google.common.base.Predicate;
 import com.primihub.biz.entity.base.BaseResultEnum;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.*;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.ResponseMessage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,31 +25,22 @@ import java.util.Random;
 @EnableSwaggerBootstrapUI
 public class SwaggerConfig {
 
-    @Bean
-    public Docket controllerApi() {
-        List<ResponseMessage> responseMessageList = new ArrayList<>();
+    private static ApiInfo apiInfo;
+
+    private static List<ResponseMessage> responseMessageList =  new ArrayList<>();
+
+    private static List<Parameter> params = new ArrayList<>();
+
+    static {
+        apiInfo = new ApiInfoBuilder()
+                .title("PrimiHub Platform 平台接口文档")
+                .description("基于多方安全计算、联邦学习、可信计算等技术自主研发的分布式隐私计算平台。涵盖隐私求交、联合建模、联合统计、匿踪查询、数据资源管理、算法容器管理、异构平台互联互通等功能，满足各场景下的数据价值流通需求。")
+                .contact(new Contact("PrimiHub", "https://primihub.com/", "business@primihub.com"))
+                .version("1.6.12")
+                .build();
         for (BaseResultEnum value : BaseResultEnum.values()) {
             responseMessageList.add(new ResponseMessageBuilder().code(value.getReturnCode()).message(value.getMessage()).build());
         }
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder()
-                        .title("PrimiHub Platform 平台接口文档")
-                        .description("基于多方安全计算、联邦学习、可信计算等技术自主研发的分布式隐私计算平台。涵盖隐私求交、联合建模、联合统计、匿踪查询、数据资源管理、算法容器管理、异构平台互联互通等功能，满足各场景下的数据价值流通需求。")
-                        .contact(new Contact("PrimiHub", "https://primihub.com/", "business@primihub.com"))
-                        .version("1.6.12")
-                        .build())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.primihub"))
-                .paths(PathSelectors.any())
-                .build()
-                .globalResponseMessage(RequestMethod.GET,responseMessageList)
-                .globalResponseMessage(RequestMethod.POST,responseMessageList)
-                .globalOperationParameters(getParameter());
-    }
-
-
-    private List<Parameter> getParameter() {
-        List<Parameter> params = new ArrayList<>();
         params.add(new ParameterBuilder().name("token")
                 .description("请求令牌")
                 .modelRef(new ModelRef("String"))
@@ -70,6 +59,88 @@ public class SwaggerConfig {
                 .parameterType("header")
                 .defaultValue((new Random().nextInt(900) + 100)+"")
                 .required(false).build());
-        return params;
     }
+
+    @Bean
+    public Docket allApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+
+    @Bean
+    public Docket sysApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller.sys"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("sys 系统接口")
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+    @Bean
+    public Docket dataApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller.data"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("data 业务接口")
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+
+    @Bean
+    public Docket testApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller.test"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("test 测试接口")
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+
+    @Bean
+    public Docket shareApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller.share"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("share 协同接口")
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+
+    @Bean
+    public Docket scheduleApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.primihub.application.controller.schedule"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("schedule 调度接口")
+                .globalResponseMessage(RequestMethod.GET,responseMessageList)
+                .globalResponseMessage(RequestMethod.POST,responseMessageList)
+                .globalOperationParameters(params);
+    }
+
 }
--- primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataResourceService.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/service/data/DataResourceService.java
@@ -339,6 +339,7 @@ public class DataResourceService {
         dataResourcePrRepository.deleteResource(resourceId);
         dataResource.setIsDel(1);
         dataResource.setResourceState(1);
+        fusionResourceService.saveResource(organConfiguration.getSysLocalOrganId(),findCopyResourceList(dataResource.getResourceId(), dataResource.getResourceId()));
         singleTaskChannel.input().send(MessageBuilder.withPayload(JSON.toJSONString(new BaseFunctionHandleEntity(BaseFunctionHandleEnum.SINGLE_DATA_FUSION_RESOURCE_TASK.getHandleType(),dataResource))).build());
         return BaseResultEntity.success("删除资源成功");
     }
