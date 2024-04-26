ba02c2e1bd8fa9b5110c30fd5b4e11e72e8693ee
quhaiyue <634385706@qq.com>
2024-04-26 10:24:02 +0800

    fix: no action button for the node to be reviewed

--- primihub-webconsole/vue.config.js
+++ primihub-webconsole/vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     proxy: {
       '/dev-api': {
-        target: '',
+        target: 'http://192.168.99.13:32733',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
