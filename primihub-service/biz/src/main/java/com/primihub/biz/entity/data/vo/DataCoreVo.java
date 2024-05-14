4a086ac8d0f3985950ae7af037c9442c7e632c0a
Terrence <terrence1337@126.com>
2024-05-14 16:44:59 +0800

    feat dataCore sql

--- primihub-service/biz/src/main/java/com/primihub/biz/entity/data/vo/DataCoreVo.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/entity/data/vo/DataCoreVo.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DataCoreVo {
-    private Double score;
     private String scoreModelType;
+    private Double score;
 }
--- primihub-service/biz/src/main/java/com/primihub/biz/repository/secondarydb/data/DataCoreRepository.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/repository/secondarydb/data/DataCoreRepository.java
@@ -2,15 +2,16 @@ package com.primihub.biz.repository.secondarydb.data;
 
 import com.primihub.biz.entity.data.po.DataCore;
 import com.primihub.biz.entity.data.vo.DataCoreVo;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
 public interface DataCoreRepository {
-    // todo
-    Set<DataCore> selectExistentDataCore(Set<String> fieldValueSet);
+    Set<DataCore> selectExistentDataCore(@Param("list") Set<String> fieldValueSet);
 
-    Set<DataCore> selectDataCoreFromIdNum(Set<String> idNumSet, String scoreType);
-    Set<DataCoreVo> selectDataCoreWithScore(String scoreType);
+    Set<DataCore> selectDataCoreFromIdNum(@Param("list") Set<String> idNumSet, @Param("scoreType") String scoreType);
+
+    Set<DataCoreVo> selectDataCoreWithScore(@Param("scoreType") String scoreType);
 }
