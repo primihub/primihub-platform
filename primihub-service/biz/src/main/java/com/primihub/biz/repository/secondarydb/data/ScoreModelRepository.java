00688b13ed618bb327f8ef5c8500e2486ca231dd
Terrence <terrence1337@126.com>
2024-05-16 19:45:51 +0800

    fix score model sql

--- primihub-service/biz/src/main/java/com/primihub/biz/repository/secondarydb/data/ScoreModelRepository.java
+++ primihub-service/biz/src/main/java/com/primihub/biz/repository/secondarydb/data/ScoreModelRepository.java
@@ -1,11 +1,12 @@
 package com.primihub.biz.repository.secondarydb.data;
 
 import com.primihub.biz.entity.data.po.ScoreModel;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Set;
 
 public interface ScoreModelRepository {
     Set<ScoreModel> selectAll();
 
-    ScoreModel selectScoreModelByScoreTypeValue(String scoreTypeValue);
+    ScoreModel selectScoreModelByScoreTypeValue(@Param("type") String scoreTypeValue);
 }
