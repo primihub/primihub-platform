dbe29bd3e06b1f485bc7ba46d65b79cecd6c163f
quhaiyue <634385706@qq.com>
2024-05-24 18:14:31 +0800

    fix: taskcanvas one node on edge

--- primihub-webconsole/src/components/TaskCanvas/index.vue
+++ primihub-webconsole/src/components/TaskCanvas/index.vue
@@ -423,7 +423,6 @@ export default {
               radius: 8
             }
           },
-          allowMulti:false,
           anchor: 'center',
           connectionPoint: 'anchor',
           allowBlank: false,
@@ -443,7 +442,34 @@ export default {
               },
               zIndex: 0
             })
-          }
+          },
+          validateConnection({ sourceCell, targetCell, targetMagnet }) {
+            if (!targetMagnet) {
+              return false
+            }
+
+            if (targetMagnet.getAttribute('port-group') !== 'in') {
+              return false
+            }
+
+            if (targetCell) {
+              const targetId = targetCell.id
+              const sourceId = sourceCell.id
+              const model = targetCell.model
+              if (model){
+                const incomings = model.incomings
+                const outgoings = model.outgoings
+                if (incomings[targetId] && incomings[targetId].length > 0){
+                  return false
+                }
+
+                if (outgoings[sourceId] && outgoings[sourceId].length > 1){
+                  return false
+                }
+              }
+            }
+            return true
+        },
         },
         selecting: {
           enabled: true,
