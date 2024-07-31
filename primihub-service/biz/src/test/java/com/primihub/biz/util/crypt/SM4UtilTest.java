5d5f8a4fd937bfd652e2e490ca27c12968629de6
Terrence <terrence1337@126.com>
2024-07-31 16:42:33 +0800

    fix data_imei save sql

--- primihub-service/biz/src/test/java/com/primihub/biz/util/crypt/SM4UtilTest.java
+++ primihub-service/biz/src/test/java/com/primihub/biz/util/crypt/SM4UtilTest.java
@@ -15,12 +15,12 @@ public class SM4UtilTest {
     // requestRefId=SJSREQ_201601010809108632A&secretId=KFZQpn74WFkmLPx3gnP
 
     public static void main(String[] args) {
-        String s = test1("SJSREQ_201601010809108632A", secretId);
+        String s = test1("123456", secretId);
         System.out.println("sign 加密后: " + s);
 
         Map<String, Object> map = new HashMap<String, Object>();
 
-        String sm3PhoneNum = SM3Util.encrypt("15871801048");
+        String sm3PhoneNum = SM3Util.encrypt("869167065109479");
         System.out.println("sm3phonenum : " + sm3PhoneNum);
         map.put("param", new HashMap<String, String>() {{
             put("mobile", sm3PhoneNum);
@@ -28,11 +28,11 @@ public class SM4UtilTest {
         }});
         String s1 = test2(map);
         System.out.println("参数加密后: " + s1);
-        try {
-            test3("a684c92f509bafed25b28785b61793b35dcc0e25f567d3d58b1420619cb6427d");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            test3("a684c92f509bafed25b28785b61793b35dcc0e25f567d3d58b1420619cb6427d");
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     public static String test1(String requestRefId, String secretId) {
