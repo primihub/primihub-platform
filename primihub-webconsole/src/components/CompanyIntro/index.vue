04f24d0314ebfc1dd4ecd1da0e4d6e23a91523d6
terrence <terrence1337@126.com>
2024-10-21 11:43:45 +0800

    fix login image height

--- primihub-webconsole/src/components/CompanyIntro/index.vue
+++ primihub-webconsole/src/components/CompanyIntro/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="loaded" class="intro">
     <div class="intro-container">
-      <h1><img v-if="isShowLogo && loginLogoUrl" class="logo" :src="loginLogoUrl" alt="logo"></h1>
+      <img v-if="isShowLogo && loginLogoUrl" class="logo" :src="loginLogoUrl" alt="logo">
       <div v-html="description" />
     </div>
   </div>
@@ -40,8 +40,6 @@ export default {
   .logo{
     height: 50px;
     width: auto;
-    display: block;
-    margin: 0 auto;
   }
 }
 </style>
